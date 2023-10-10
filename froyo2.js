d5c73619e5f1d5e7d900571e6b0ea3a018ead67f
quiryat zavala <qzavala2014@gmail.com>
2023-10-10 14:42:08 -0700

    froyo assigment

--- froyo2.js
+++ froyo2.js
@@ -1,17 +1,24 @@
 const userInputString = prompt(
-    "Please enter some froyo flavors separated by commas.",
-    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
-  );
+  "Please enter some froyo flavors separated by commas.",
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
 //const inputstring =  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 
-function getFroyo(str){
-    const list = str.split(",");
-    console.log(list)
-    const object = {}
-    
-    for (let i =0; i < list.length; i++){
-        const liveView = list[i]
-        console.log(list[i])
+function getFroyo(str) {
+  const list = str.split(",");
+  console.log(list);
+  const object = {};
+
+  for (let i = 0; i < list.length; i++) {
+    const liveView = list[i];
+    console.log(list[i]);
+    if (object[liveView]) {
+      object[liveView] += 1;
+    } else {
+      object[liveView] = 1;
     }
+  }
+  return object;
 }
-console.table(getFroyo(userInputString))
+
+console.table(getFroyo(userInputString));
